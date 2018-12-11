c2980fcbce3051ea90c8a07b22c9c76d231194f2
zhaokh <wwwzhaokh@126.com>
2018-12-12 00:01:44 +0800

    应用items

--- scrapy_fd/spiders/jd.py
+++ scrapy_fd/spiders/jd.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_fd.items import ScrapyFdItem
 
 
 class JdSpider(scrapy.Spider):
     name = 'jd'
-    allowed_domains = ['https://list.jd.com/list.html?cat=737,794,965']
-    start_urls = ['http://https://list.jd.com/list.html?cat=737,794,965/']
-
+    allowed_domains = ["dmoz.org"]
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            #'https://list.jd.com/list.html?cat=737,794,965/',
+            "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+            "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.logger.info('A response from %s just arrived!!!!!!!!!!!!!', response.url) 
+        for sel in response.xpath('//ul/li'):
+            item = ScrapyFdItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
         pass
