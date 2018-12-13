acc197225f6e8d23407a25f907671f66297121cc
zhaokh <wwwzhaokh@126.com>
2018-12-13 23:01:23 +0800

    自己代码，尝试例子，但是有错误

--- scrapy_fd/items.py
+++ scrapy_fd/items.py
@@ -11,7 +11,7 @@ import scrapy
 class ScrapyFdItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    title = scrapy.Field()
+    name = scrapy.Field()
     link = scrapy.Field()
     desc = scrapy.Field()
     pass
--- scrapy_fd/spiders/jd.py
+++ scrapy_fd/spiders/jd.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy_fd.items import ScrapyFdItem
 
 
@@ -8,19 +9,41 @@ class JdSpider(scrapy.Spider):
     allowed_domains = ["dmoz.org"]
     def start_requests(self):
         urls = [
-            #'https://list.jd.com/list.html?cat=737,794,965/',
-            "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-            "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+            'https://www.jd.com/allSort.aspx',
+            #"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+            #"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_category)
 
     def parse(self, response):
         self.logger.info('A response from %s just arrived!!!!!!!!!!!!!', response.url) 
-        for sel in response.xpath('//ul/li'):
+        """
+        for sel in response.xpath('//ul/li[@class="gl-item"]'):
             item = ScrapyFdItem()
-            item['title'] = sel.xpath('a/text()').extract()
+            item['name'] = sel.xpath('div[@class="p-name"]//a//em/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
             yield item
         pass
+        """
+
+    def parse_category(self, response):
+        """获取分类页"""
+        try:
+            texts = response.xpath('//div[@class="category-item m"]/div[@class="mc"]/div[@class="items"]/dl/dd/a').extract()
+            for text in texts:
+                items = re.findall(r'<a href="(.*?)" target="_blank">(.*?)</a>', text)
+                for item in items:
+                    if item[0].split('.')[0][2:] in key_word:
+                        if item[0].split('.')[0][2:] != 'list':
+                            yield scrapy.Request(url='https:' + item[0], callback=self.parse_category)
+                        else:
+                            categoriesItem = CategoriesItem()
+                            categoriesItem['name'] = item[1]
+                            categoriesItem['url'] = 'https:' + item[0]
+                            categoriesItem['_id'] = item[0].split('=')[1].split('&')[0]
+                            yield categoriesItem
+                            yield scrapy.Request(url='https:' + item[0], callback=self.parse_list)
+        except Exception as e:
+            print('error:', e)
